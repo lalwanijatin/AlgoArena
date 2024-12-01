88cae24b8747b642b3a7cec6fc3501bc90605265
Jatin Lalwani <jatinlalwani33@gmail.com>
2024-12-02 02:48:42 +0530

    Update Application entrypoint

--- algo-arena-be/src/main/java/com/algoarena/AlgoarenaApplication.java
+++ algo-arena-be/src/main/java/com/algoarena/AlgoarenaApplication.java
@@ -1,14 +1,15 @@
 package com.algoarena;
 
+import com.algoarena.api.Problem;
 import com.algoarena.boilerplate.CodeGenerator;
-import com.algoarena.boilerplate.JavaCodeGenerator;
-import com.algoarena.boilerplate.JavaScriptCodeGenerator;
-import com.algoarena.boilerplate.ProblemStructureReader;
+import com.algoarena.problems.ProblemWithStatus;
 import com.algoarena.recon.SubmissionRecon;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class AlgoarenaApplication {
@@ -18,9 +19,15 @@ public class AlgoarenaApplication {
 	}
 
 	@Bean
-	public CommandLineRunner run(JavaCodeGenerator javaCodeGenerator, CodeGenerator codeGenerator, SubmissionRecon submissionRecon, JavaScriptCodeGenerator javaScriptCodeGenerator, ProblemStructureReader problemStructureReader){
+	public CommandLineRunner run(SubmissionRecon submissionRecon, Problem problem, CodeGenerator codeGenerator){
 		return (args) -> {
+			// Start Submission Recon in a separate Thread
 			submissionRecon.initiateReconProcess();
+
+			// Generate Boilerplate code for all the problems present in problem table
+			List<ProblemWithStatus> problemList = problem.getProblemWithStatus();
+			for(ProblemWithStatus problemWithStatus : problemList)
+				codeGenerator.generate(problemWithStatus.getName());
 		};
 	}
 
